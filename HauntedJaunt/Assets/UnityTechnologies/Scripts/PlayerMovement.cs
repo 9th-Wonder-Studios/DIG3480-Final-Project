492386c5e7db2f9d14490438ca19387cf427cc0f
Chandler Guzman <ninthewander@gmail.com>
2025-12-01 14:48:21 -0500

    tutorials part 7-9 modifications
    finalized project, added a splash screen logo for our studio & a game icon

--- HauntedJaunt/Assets/UnityTechnologies/Scripts/PlayerMovement.cs
+++ HauntedJaunt/Assets/UnityTechnologies/Scripts/PlayerMovement.cs
@@ -9,15 +9,15 @@ public class PlayerMovement : MonoBehaviour
     public float turnSpeed = 20f;
     Animator m_Animator;
     Rigidbody m_Rigidbody;
-
-    // Start is called before the first frame update
+    private AudioSource m_AudioSource; 
+    
     void Start()
     {
         m_Animator = GetComponent<Animator>();
         m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
     }
-
-    // Update is called once per frame
+    
     void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
@@ -30,6 +30,18 @@ public class PlayerMovement : MonoBehaviour
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
         m_Animator.SetBool("IsWalking", isWalking);
+        
+        if (isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop();
+        }
 
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation(desiredForward);
